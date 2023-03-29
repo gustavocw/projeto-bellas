c438db2dd3c9dcdd0533a403621bb0b4393240c2
gustavocw <guszfx@gmail.com>
2023-03-29 12:38:22 +0100

    btns

--- src/components/Question/QuestionCard.tsx
+++ src/components/Question/QuestionCard.tsx
@@ -50,10 +50,10 @@ import {
               </FormControl>
               <FormControl>
                 <FormLabel>Oque procura no site ?</FormLabel>
-                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} >HOMENS</Button>
-                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} >MULHERES</Button>
-                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} >TRANS</Button>
-                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} >CASAIS</Button>
+                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} my={'2'} >HOMENS</Button>
+                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} my={'2'} >MULHERES</Button>
+                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} my={'2'} >TRANS</Button>
+                <Button bg={'pink.500'} color={'#fff'} size={'sm'} mx={'2'} my={'2'} >CASAIS</Button>
               </FormControl>
             </ModalBody>
   
