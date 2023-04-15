b9ca96a062519cf9fabdc637bfb25787d6a1ed5d
gustavocw <guszfx@gmail.com>
2023-04-15 15:36:19 +0100

    edit

--- src/client/painel/autorizados/detalhes/Detalhes.tsx
+++ src/client/painel/autorizados/detalhes/Detalhes.tsx
@@ -1,6 +1,6 @@
 import "./detalhes.css";
-import Footer from "../../../components/Footer/Footer";
-import Header from "../../../components/Header/Header";
+import Footer from "../../../../components/Footer/Footer";
+import Header from "../../../../components/Header/Header";
 import DetailsUser from "./DetailsUser";
 
 const DetalhesPage = () => {
